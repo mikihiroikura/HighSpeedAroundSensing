943dc17d7b787b5b570fa3f2e3adaa284c175d0e
mikihiroikura <miki.ut.sikepuri@gmail.com>
2020-11-17 03:23:32 +0900

    dr=0.5の時計算範囲が倍になっていた

--- sample/DebugImgs/main.cpp
+++ sample/DebugImgs/main.cpp
@@ -118,13 +118,13 @@ int main() {
 			mass = 0;
 			moment_x = 0;
 			moment_y = 0;
-			forend = (int)(3.15 * 2 * (double)r * dtheta / 360);
 			rs = (double)r / 2;
+			forend = (int)(3.15 * 2 * rs * dtheta / 360);
 			for (size_t i = 0; i < forend; i++)
 			{
 				x = (int)(960 + (double)rs * cos((double)i / rs + deg - dtheta / 2 / 360 * 3.1415));
 				y = (int)(540 + (double)rs * sin((double)i / rs + deg - dtheta / 2 / 360 * 3.1415));
-				//detectarea.data[y * detectarea.cols + x] = 255;
+				detectarea.data[y * detectarea.cols + x] = 255;
 				if ((int)rei2.data[y * step + x * elem + 1] > 0)
 				{
 					//cout << x << "," << y << " bps " << (int)rei2.data[y * rei2.step + x * rei2.elemSize() + 1] << endl;
@@ -141,32 +141,32 @@ int main() {
 			}
 		}
 //#pragma omp parallel for
-//		for (int r = rstart; r < 430; r++)
-//		{
-//			cout << omp_get_thread_num() << endl;
-//			mass = 0;
-//			moment_x = 0;
-//			moment_y = 0;
-//			forend = (int)(3.15 * 2 * (double)r * dtheta / 360);
-//			for (size_t i = 0; i < forend; i++)
-//			{
-//				x = (int)(960 + (double)r * cos((double)i / r + deg - dtheta / 2 / 360 * 3.1415));
-//				y = (int)(540 + (double)r * sin((double)i / r + deg - dtheta / 2 / 360 * 3.1415));
-//				//detectarea.data[y * rei.cols + x] = 255;
-//				if ((int)rei2.data[y * step + x * elem + 1] > 0)
-//				{
-//					//cout << x << "," << y << " bps " << (int)rei.data[y * rei.step + x * rei.elemSize() + 1] << endl;
-//					mass += rei2.data[y * step + x * elem + 1];
-//					moment_x += rei2.data[y * step + x * elem + 1] * x;
-//					moment_y += rei2.data[y * step + x * elem + 1] * y;
-//				}
-//			}
-//			if (mass > 0)
-//			{
-//				cog[r - rstart][0] = moment_x / mass;
-//				cog[r - rstart][1] = moment_y / mass;
-//			}
-//		}
+		//for (int r = rstart; r < 430; r++)
+		//{
+		//	cout << omp_get_thread_num() << endl;
+		//	mass = 0;
+		//	moment_x = 0;
+		//	moment_y = 0;
+		//	forend = (int)(3.15 * 2 * (double)r * dtheta / 360);
+		//	for (size_t i = 0; i < forend; i++)
+		//	{
+		//		x = (int)(960 + (double)r * cos((double)i / r + deg - dtheta / 2 / 360 * 3.1415));
+		//		y = (int)(540 + (double)r * sin((double)i / r + deg - dtheta / 2 / 360 * 3.1415));
+		//		detectarea.data[y * rei.cols + x] = 255;
+		//		if ((int)rei2.data[y * step + x * elem + 1] > 0)
+		//		{
+		//			//cout << x << "," << y << " bps " << (int)rei.data[y * rei.step + x * rei.elemSize() + 1] << endl;
+		//			mass += rei2.data[y * step + x * elem + 1];
+		//			moment_x += rei2.data[y * step + x * elem + 1] * x;
+		//			moment_y += rei2.data[y * step + x * elem + 1] * y;
+		//		}
+		//	}
+		//	if (mass > 0)
+		//	{
+		//		cog[r - rstart][0] = moment_x / mass;
+		//		cog[r - rstart][1] = moment_y / mass;
+		//	}
+		//}
 		if (!QueryPerformanceCounter(&end)) { return 0; }
 		logtime = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
 		cout << logtime << endl;
