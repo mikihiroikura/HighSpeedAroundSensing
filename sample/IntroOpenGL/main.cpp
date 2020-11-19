070cec20f842ec68f468ccfeed1c6742abae6654
mikihiroikura <miki.ut.sikepuri@gmail.com>
2020-11-19 21:38:15 +0900

    OpenGLの練習，エラーで出力されない

--- sample/IntroOpenGL/main.cpp
+++ sample/IntroOpenGL/main.cpp
@@ -1,7 +1,139 @@
-#include <GL/>
+#include <iostream>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
+#include <glm/gtc/matrix_transform.hpp> 
+#include <glm/gtx/transform.hpp>
 
+using namespace std;
+
+// constants
+const unsigned int window_width = 1280;
+const unsigned int window_height = 720;
+GLFWwindow* window;
+
+//vbo
+GLuint vbo;
+float position[100][100][3];
+
+//imgui
+float rotate_x = 0.0, rotate_y = 0.0;
+float translate_z = -3.0;
 
 int main() {
+    //GLFWの初期化
+    if (glfwInit() == GL_FALSE)
+    {
+        std::cerr << "Can't initilize GLFW" << std::endl;
+        return 1;
+    }
 
+    //Windowの作成
+    window = glfwCreateWindow(window_width, window_height, "Cuda GL Interop (VBO)", NULL, NULL);
+    if (window == nullptr)
+    {
+        std::cerr << "Can't create GLFW window." << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    //WindowをOpenGLの対象にする
+    glfwMakeContextCurrent(window);
+    //MakeCOntextcurrentの後に行わないと失敗するらしい
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Can't initilize GLEW" << std::endl;
+        return 1;
+    }
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);   //背景色の指定
+    glDisable(GL_DEPTH_TEST);
+
+    //positionの初期化
+    for (size_t i = 0; i < 100; i++)
+    {
+        for (size_t j = 0; j < 100; j++)
+        {
+            position[i][j][0] = (float)i * 0.01;
+            position[i][j][1] = (float)j * 0.01;
+            position[i][j][2] = 0.0;
+        }
+    }
+
+    //頂点バッファオブジェクト
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(position), position);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // viewport
+    glViewport(0, 0, window_width, window_height);
+
+    // projection
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (GLfloat)window_width / (GLfloat)window_height, 0.1, 10.0);
+
+    //imguiの初期化
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
+    //ここにループを書く
+    while (glfwWindowShouldClose(window) == GL_FALSE)
+    {
+        // run the cuda part
+        /*runCuda(&cuda_vbo_resource);*/
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glLoadIdentity();
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        gluLookAt(0, 0, 0, 0, 0, 1, 0, -1, 0); //これでカメラの上向きの軸を--y方向にすることで上下を合わせる
+        glRotated(rotate_x, 1, 0, 0);
+        glRotated(rotate_y, 0, 1, 0);
+        glTranslatef(0, 0, translate_z);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glVertexPointer(3, GL_FLOAT, 0, 0);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glColor3f(1.0, 0.0, 0.0);
+        glDrawArrays(GL_POINTS, 0, 100*100);
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glPopMatrix();
+
+        glfwPollEvents();
+
+        //start imgui
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("hello world");
+        ImGui::Text("This is useful text");
+        ImGui::DragFloat("rotate x", &rotate_x);
+        ImGui::DragFloat("rotate y", &rotate_y);
+        ImGui::DragFloat("trans z", &translate_z);
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+        glfwSwapBuffers(window);
+    }
+
+    glBindBuffer(1, vbo);
+    glDeleteBuffers(1, &vbo);
+
+    return 0;
 }
