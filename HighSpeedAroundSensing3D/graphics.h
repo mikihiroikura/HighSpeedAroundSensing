182220300f508e05cac711a181ea88f0861b2fc0
mikihiroikura <miki.ut.sikepuri@gmail.com>
2020-12-29 22:44:23 +0900

    OpenGL状に現在のRPM,modeを出力する

--- HighSpeedAroundSensing3D/graphics.h
+++ HighSpeedAroundSensing3D/graphics.h
@@ -16,13 +16,15 @@
 #include <Windows.h>
 #include "params.h"
 
-extern void drawGL(LSM* lsm, Logs* logs, bool* flg);
 extern void drawGL_one(double* pts, int* lsmshowid);
-extern void drawGL2(bool* flg, double* pts, int* lsmshowid);
 extern void finishGL();
 extern void initGL();
 
 const double safe_area = 1.5, danger_area = 0.2, zero_area = 0.01;
+
+//•\¦‚µ‚½‚¢î•ñ
+extern char mode; //DDMotor‚Ì§Œäƒ‚[ƒh
+extern int rpm; //DDMotor‚Ì‰ñ“]”
 
 
 #endif // GRAPHICS_H
