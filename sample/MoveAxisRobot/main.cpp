131ee2a078f56931f6738bff37287291c7864202
mikihiroikura <miki.ut.sikepuri@gmail.com>
2021-02-05 22:10:55 +0900

    単軸ロボット自動動作コマンドの実装

--- sample/MoveAxisRobot/main.cpp
+++ sample/MoveAxisRobot/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #define READBUFFERSIZE 256
 
 void wait_QueryPerformance(double finishtime, LARGE_INTEGER freq);
+void Read_Reply_toEND(RS232c* robot);
 
 int main(int argc, char* argv[]) {
 	RS232c axisrobot;
@@ -18,6 +19,7 @@ int main(int argc, char* argv[]) {
 	char modes[][10] = { "@SRVO", "@START", "@ORG" };
 	char command[256] = "";
 	double maintime = 0;
+	bool comresult;
 
 	if (!axisrobot.Connect("COM6", 38400, 8, ODDPARITY, 0, 0, 0, 20000, 20000)) {
 		cout << "No connect" << endl;
@@ -28,32 +30,33 @@ int main(int argc, char* argv[]) {
 	if (!QueryPerformanceCounter(&start)) return 0;
 	snprintf(command, READBUFFERSIZE, "%s%d.1\r\n", modes[0], 1);
 	axisrobot.Send(command);
-	axisrobot.Read_CRLF(buf, 256);
+	Read_Reply_toEND(&axisrobot);
 	cout << "SERVO ON" << endl;
 	wait_QueryPerformance(1, freq);
 	axisrobot.Send("@?D18.1\r\n");
-	axisrobot.Read_CRLF(reply, 256);
-	axisrobot.Read_CRLF(buf, 256);
+	Read_Reply_toEND(&axisrobot);
 
 	snprintf(command, READBUFFERSIZE, "%s.1\r\n", modes[2]);
 	axisrobot.Send(command);
-	axisrobot.Read_CRLF(buf, 256);
-	axisrobot.Read_CRLF(buf, 256);
 	cout << "ORG START" << endl;
 	memset(command, '\0', READBUFFERSIZE);
+	Read_Reply_toEND(&axisrobot);
+	cout << "ORG STOP" << endl;
 	wait_QueryPerformance(2.0, freq);
 
 	
 	
 	srand(time(NULL));
-	int pointno;
+	int pointno = -1, pointno_old = -1;
 	//wait_QueryPerformance(0.1, freq);
 	for (size_t i = 0; i < 5; i++)
 	{
-		pointno = rand()%15 + 1;
+		while (pointno == pointno_old) {
+			pointno = rand() % 15 + 1;
+		}
 		snprintf(command, READBUFFERSIZE, "%s%d.1\r\n", modes[1], pointno);
 		axisrobot.Send(command);
-		axisrobot.Read_CRLF(buf, 256);
+		/*axisrobot.Read_CRLF(buf, 256);
 		axisrobot.Read_CRLF(buf, 256);
 		while (true)
 		{
@@ -62,14 +65,19 @@ int main(int argc, char* argv[]) {
 			if (reply[0] == 'D' && reply[6] == '0') break;
 			axisrobot.Read_CRLF(reply, 256);
 			if (reply[0] == 'D' && reply[6] == '0') break;
-		}
+		}*/
+		Read_Reply_toEND(&axisrobot);
+		pointno_old = pointno;
 	}
-	axisrobot.Send("@?D18.1\r\n");
-	axisrobot.Read_CRLF(reply, 256);
-	axisrobot.Read_CRLF(buf, 256);
+
+	comresult = axisrobot.Send("@S_17.1=30\r\n");
+	Read_Reply_toEND(&axisrobot);
+	comresult = axisrobot.Send("@START17#P20000.1\r\n");
+	Read_Reply_toEND(&axisrobot);
 
 	snprintf(command, READBUFFERSIZE, "%s%d.1\r\n", modes[0], 0);
 	axisrobot.Send(command);
+	Read_Reply_toEND(&axisrobot);
 	memset(command, '\0', READBUFFERSIZE);
 	cout << "SERVO OFF" << endl;
 }
@@ -82,5 +90,16 @@ void wait_QueryPerformance(double finishtime, LARGE_INTEGER freq) {
 	{
 		QueryPerformanceCounter(&waitstop);
 		waittime = (double)(waitstop.QuadPart - waitstart.QuadPart) / freq.QuadPart;
+	}
+}
+
+void Read_Reply_toEND(RS232c *robot) {
+	char bufreply[256];
+	while (true)
+	{
+		robot->Read_CRLF(bufreply, 256);
+		if (bufreply[0] == 'E' || bufreply[0] == 'O') {
+			break;
+		}
 	}
 }
